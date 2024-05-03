26154480f9ab8cd79b9288553c4c18256b65ac64
orels1 <ping@orels.sh>
2024-05-03 19:47:07 +0400

    improved columns on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -57,7 +57,7 @@ export default function Home() {
     <div className="flex flex-col w-full">
       <h1 className="text-3xl font-bold">orels1 types words...</h1>
       <h2 className="text-md font-light text-secondary-foreground">...and you read them</h2>
-      <div className="grid grid-cols-3 mt-6 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6 gap-6">
         {content.map((post) => (
           <div key={post.id} className="rounded-xl ring-1 dark:ring-white/10 ring-zinc-300 p-4 flex flex-col gap-4">
             <div className="rounded-xl overflow-hidden aspect-video relative">
