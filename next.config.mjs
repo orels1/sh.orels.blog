7d6553ee6eacb514ab8cad81f8eda89fb9584377
orels1 <ping@orels.sh>
2024-05-04 16:58:42 +0400

    moved covers into the repo

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'blog.orels.sh',
         port: '',
-        pathname: '/content/images/**',
+        pathname: '/img/**',
       },
     ],
   },
